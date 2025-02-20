ee7db7976a4d05729b2563896e2a7390c333442f
vishalbv <vishalbv23@gmail.com>
2025-02-21 00:00:15 +0530

    added css

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "4rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1500px",
       },
     },
     extend: {
